f2e49ae1e4dd5fc1a290a50b1f4110f949f8fdec
mohamuhsin <muhsinohoho@outlook.com>
2023-07-26 15:29:42 +0300

    completed Handlers

--- src/components/AddUser/UserInput.js
+++ src/components/AddUser/UserInput.js
@@ -10,20 +10,30 @@ const UserInput = () => {
     console.log("RESET");
   };
 
-  const inputChangeHandler = (input, value) = {
-
-  }
+  const inputChangeHandler = (input, value) => {};
 
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input onChange={(event) => inputChangeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("current-savings", event.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input onChange={(event) => inputChangeHandler('yearly-contribution', event.target.value)} type="number" id="yearly-contribution" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearly-contribution", event.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -31,11 +41,23 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} type="number" id="expected-return" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} type="number" id="duration" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            type="number"
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
