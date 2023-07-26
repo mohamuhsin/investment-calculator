15a794c0f7ba293dccf3948c93504e79379167a0
mohamuhsin <muhsinohoho@outlook.com>
2023-07-26 14:54:35 +0300

    added a handler function

--- src/components/AddUser/UserInput.js
+++ src/components/AddUser/UserInput.js
@@ -10,16 +10,20 @@ const UserInput = () => {
     console.log("RESET");
   };
 
+  const inputChangeHandler = (input, value) = {
+
+  }
+
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input onChange={(event) => inputChangeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input onChange={(event) => inputChangeHandler('yearly-contribution', event.target.value)} type="number" id="yearly-contribution" />
         </p>
       </div>
       <div className="input-group">
@@ -27,11 +31,11 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} type="number" id="expected-return" />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} type="number" id="duration" />
         </p>
       </div>
       <p className="actions">
